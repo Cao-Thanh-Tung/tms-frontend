2ed5a9ca1798e3a401b8f8502a068085c565a725
Nguyen Van Nam <nam.nv205106@gmail.com>
2024-02-05 01:31:11 +0700

    map

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import MapVue from "./components/Map.vue";
+// import LoginVue from "./components/Login.vue";
 </script>
 <template>
-  <MapVue />
+  <MapVue/>
 </template>
