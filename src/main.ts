9106398346cfe1013a429ad2bc41ad6e7b75ded5
Nguyen Van Nam <nam.nv205106@gmail.com>
2024-02-21 15:17:14 +0700

    stash commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import {createApp} from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue'
+import '@/style.css'
+import Ant from 'ant-design-vue'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import axios from 'axios';
+export * from "@/api";
+export * from "@/configuration";
+const baseUrl = import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL = baseUrl;
+createApp(App).use(store).use(router).use(Ant).mount('#app')
 
-export * from "./api";
-export * from "./configuration";
-
-createApp(App).mount('#app')
